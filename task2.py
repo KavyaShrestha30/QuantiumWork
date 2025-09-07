e63bc6a3a1b61654b168ecb9c36551620f4398ca
KavyaShrestha30 <shresthakavya26@gmail.com>
2025-09-07 14:40:37 +0100

    Visualized csv data using Dash

--- task2.py
+++ task2.py
@@ -1,5 +1,9 @@
 from pathlib import Path
+from dash import Dash, html, dcc
+import plotly.express as px
 import pandas as pd
+
+app = Dash()
 
 folder = Path("data")   # e.g., Path("data")
 files = sorted(folder.glob("*.csv"))
@@ -25,3 +29,44 @@ newdf = pd.DataFrame({
 
 # optional: write it back out
 newdf.to_csv("all_data.csv", index=False)
+
+markdown_text = '''
+### Markdown in Dash
+
+Dash apps can be written in Markdown.
+Dash uses the [CommonMark](http://commonmark.org/)
+specification of Markdown.
+Check out their [60 Second Markdown Tutorial](http://commonmark.org/help/)
+if this is your first introduction to Markdown!
+'''
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+fig = px.line(newdf, x="date", y="Sales")
+
+app.layout = html.Div(children=[
+    html.H1(children='Sales for Pink Morsel',style={'textAlign': 'center', 'color': '#7FDBFF'}),
+
+    html.Div(children='Dash: A web application framework for your data.'),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    ),
+    
+    dcc.Markdown(children = markdown_text)
+    
+])
+
+if __name__ == '__main__':
+    app.run(debug=True)
